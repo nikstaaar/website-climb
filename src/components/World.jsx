6fafbac8cec9e2c56b2e3959b70e41ce9c7db566
Your Name <niktjawalla922@gmail.com>
2023-12-15 03:18:24 +0100

    enlarge floor

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,4 +1,9 @@
-import { PerspectiveCamera, Environment, Cylinder } from '@react-three/drei'
+import {
+	PerspectiveCamera,
+	Environment,
+	Cylinder,
+	Plane,
+} from '@react-three/drei'
 import { RigidBody } from '@react-three/rapier'
 import gameStore from '../stores/gameStore'
 
@@ -24,12 +29,7 @@ export default function World() {
 	}))
 	return (
 		<>
-			<Environment
-				background
-				near={1}
-				far={1}
-				files={'/public/env/puresky_8k.hdr'}
-			/>
+			<Environment background files={'/public/env/puresky_8k.hdr'} />
 			<PerspectiveCamera makeDefault manual aspect={16 / 9} />
 			<ambientLight intensity={1} />
 			<RigidBody
@@ -41,14 +41,21 @@ export default function World() {
 			>
 				<mesh position={[0, -5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
 					<boxGeometry args={[200, 200, 1]}></boxGeometry>
-					<meshStandardMaterial roughness={0.6} color={'#202026'} />
+					<meshBasicMaterial opacity={0} transparent />
 				</mesh>
 			</RigidBody>
-			<Wall rotation={[0, Math.PI / 2, 0]} position={[100, 100, 0]}></Wall>
-			<Wall rotation={[0, Math.PI / 2, 0]} position={[-100, 100, 0]}></Wall>
-			<Wall rotation={[0, 0, Math.PI / 2]} position={[0, 100, 100]}></Wall>
-			<Wall rotation={[0, 0, Math.PI / 2]} position={[0, 100, -100]}></Wall>
-			<Wall rotation={[Math.PI / 2, 0, 0]} position={[0, 200, 0]}></Wall>
+			<Plane
+				rotation={[-Math.PI / 2, 0, 0]}
+				position={[0, -5, 0]}
+				args={[1000, 1000]}
+			>
+				<meshStandardMaterial roughness={0.6} color={'#202026'} />
+			</Plane>
+			<Wall rotation={[0, Math.PI / 2, 0]} position={[100, 95, 0]}></Wall>
+			<Wall rotation={[0, Math.PI / 2, 0]} position={[-100, 95, 0]}></Wall>
+			<Wall rotation={[0, 0, Math.PI / 2]} position={[0, 95, 100]}></Wall>
+			<Wall rotation={[0, 0, Math.PI / 2]} position={[0, 95, -100]}></Wall>
+			<Wall rotation={[Math.PI / 2, 0, 0]} position={[0, 195, 0]}></Wall>
 			{stage === 'walking' && (level === 0 || level === 1) ? (
 				<RigidBody
 					type={'fixed'}
